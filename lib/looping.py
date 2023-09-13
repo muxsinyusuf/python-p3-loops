0239ed815495990981a952317e0a1c5fc9a185f8
muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-09-13 08:58:26 +0300

    lab done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+   counter = 0
+   while counter > 0:
+    print(counter)
+    counter -= 1
+    print("Happy new Year")
     # code goes here!
-    pass
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+   int_list = [1,2,3,4,5]
+   square_list = [x ** 2 for x in int_list]
+   return square_list
+
+   # code goes here!
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+
+
